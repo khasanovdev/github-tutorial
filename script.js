f890c94a7821e2b47a1957fc237d2f6034ef12cb
kozimjon <khasanovkozimjon0198@gmail.com>
2021-07-04 18:04:01 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+// const Answer = []
+// const Name = prompt("ismiz");
+// const Age = +prompt("yoshingiz");
+
+// Answer[0] = Name;
+// Answer[1] = Age;
+// document.write(`
+
+//         Salom ${Answer[0]}. Siz ${ (2021 - Answer[1]) } - yilda tug'ilgansiz 
+// `)
+
+// const up = (str) => str.toUpperCase();
+// str = `bu katta harf bilan ${up("string")}`;
+
+// function  foo(str) {
+//         console.log( `Yozilgan matn katta harfga o'zgaradi => ${str.toUpperCase()} ` );
+// }
+
+hello = (str) => ` ${str.toUpperCase()} Hello World! `;
